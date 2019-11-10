e30f730713a20a2e858b58fdc1e97c4b73c55cb4
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-11-11 01:38:49 +0530

    comment3

--- SLibrary/src/SLibrary/Login.java
+++ SLibrary/src/SLibrary/Login.java
@@ -49,6 +49,11 @@ public class Login extends javax.swing.JFrame {
         jLabel2.setText("Password");
 
         jButton1.setText("Login");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Cancel");
 
@@ -120,6 +125,27 @@ public class Login extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+      String UserName=txtusername.getText();
+      String Pass=txtpass.getText();
+      
+      if(UserName.equals("john")&& Pass.equals("123"))
+          
+      {
+          Main m=new Main();
+          this.hide();
+          M.setVisible(true);
+      }
+      else
+      {
+          
+      }
+        
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
